09889f1190169d38775bac0a132d648814da0f0d
GentjanQeriqi <gentqeriqi96@gmail.com>
2020-09-29 16:34:57 +0200

    added files

---
+++ BE/src/main/java/com/moveengineering/ChatGroupBE/ChatGroupBeApplication.java
@@ -0,0 +1,13 @@
+package com.moveengineering.ChatGroupBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGroupBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGroupBeApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/moveengineering/ChatGroupBE/ChatGroupBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveengineering.ChatGroupBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGroupBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
