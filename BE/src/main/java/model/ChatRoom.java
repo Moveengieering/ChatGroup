a2fb68fa4814a6d7ba345a745b9845a314f595b0
ertugertshkulaku <ertugert.shkulaku1@gmail.com>
2020-09-30 16:58:21 +0200

    SomeChanges

--- BE/src/main/java/model/ChatRoom.java
+++ BE/src/main/java/model/ChatRoom.java
@@ -1,5 +1,4 @@
 package model;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,23 +6,16 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
-
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Document
-public class ChatMessage {
+public class ChatRoom {
     @Id
     private String id;
     private String chatId;
     private String senderId;
     private String recipientId;
-    private String senderName;
-    private String recipientName;
-    private String content;
-    private Date timestamp;
-    private MessageStatus status;
 }
